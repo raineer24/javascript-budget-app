d819edfa0732a63250203944bf3ece445635953d
Raineer <delaritaraineer81@gmail.com>
2019-03-17 00:12:59 +0800

    expense title value and and expense value = value working

--- js/app.js
+++ js/app.js
@@ -75,11 +75,30 @@ class UI {
             }
             this.itemID++;
             this.itemList.push(expense);
-            this.addExpense();
+            this.addExpense(expense);
             // show balance
         }
     } 
-
+    //add expense
+    addExpense(expense){
+        const div = document.createElement('div');
+        div.classList.add('expense');
+        div.innerHTML = `
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+        <h6 class="expense-title mb-0 text-uppercase list-item">-  ${expense.title}</h6>
+        <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+        <div class="expense-icons list-item">
+         <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+          <i class="fas fa-edit"></i>
+         </a>
+         <a href="#" class="delete-icon" data-id="${expense.id}">
+          <i class="fas fa-trash"></i>
+         </a>
+        </div>
+       </div>
+        `;
+       this.expenseList.appendChild(div); 
+    }
     // total expense
     totalExpense() {
         let total = 400;
